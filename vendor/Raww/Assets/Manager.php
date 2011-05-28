285df4d999e11f1e4e36491e10c6e964a70a3557
Artur Heinze <artur@d-xp.com>
2011-05-28 16:38:07 +0200

    latest status

--- vendor/Raww/Assets/Manager.php
+++ vendor/Raww/Assets/Manager.php
@@ -5,6 +5,8 @@ namespace Raww\Assets;
 
 class Manager extends \Raww\AppContainer {
   
+  public static $filters = array();
+  
   protected $assets = array();
   protected $references = array();
   protected $dumped_references = array();
@@ -41,7 +43,6 @@ class Manager extends \Raww\AppContainer {
     if($cached = $this->app['cache']->read($cache_key)) {
       return $cached;
     }
-
 
     $output    = array();
 
@@ -126,4 +127,33 @@ class Manager extends \Raww\AppContainer {
     return $content;
   }
 
+}
+
+// Filters
+
+Manager::$filters["minify_css"] = function(&$str) {
+		
+		// Colons cannot be globally matched safely because of pseudo-selectors etc.
+		$innerbrace = function($match) {
+			return preg_replace('#\s*:\s*#', ':', $match[0]);
+		};
+		
+		$str = preg_replace_callback( '#\{[^}]+\}#s', $innerbrace, trim($str));
+
+		$replacements = array(
+			'#\s{2,}#'                          => ' ',      // Remove double spaces
+			'#\s*(;|,|\{)\s*#'                  => '$1',     // Clean-up around delimiters
+			'#\s*;*\s*\}\s*#'                   => '}',      // Clean-up closing braces
+			'#[^}{]+\{\s*}#'                    => '',       // Strip empty statements
+			'#([^0-9])0[a-zA-Z%]{2}#'           => '${1}0',  // Strip unnecessary units on zeros
+			'#:(0 0|0 0 0|0 0 0 0)([;}])#'      => ':0${2}', // Collapse zero lists
+			'#(background-position):0([;}])#'   => '$1:0 0$2', // Restore any overshoot
+			'#([^\d])0(\.\d+)#'                 => '$1$2',   // Strip leading zeros on floats
+			'#(\[)\s*|\s*(\])|(\()\s*|\s*(\))#' => '${1}${2}${3}${4}',  // Clean-up bracket internal space
+			'#\s*([>~+=])\s*#'                  => '$1',     // Clean-up around combinators
+			'#\#([0-9a-f])\1([0-9a-f])\2([0-9a-f])\3#i'
+			                                    => '#$1$2$3', // Reduce Hex codes
+		);
+
+		$str = preg_replace(array_keys($replacements), array_values($replacements), $str);
 }
