297a4d4f52a090e70afac4b44bf19b48fdb2d0f9
Artur Heinze <artur@d-xp.com>
2011-06-02 21:33:16 +0200

    latest status

---
+++ vendor/Raww/Controller/Auth.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Raww\Controller;
+
+class Auth extends \Raww\Controller {
+	
+	protected $auth_login_url = '/login';
+	protected $auth_public_actions = array();
+	
+	public function before_filter(){
+
+        if(!isset($this->["session"]['Auth']) && !in_array($this->invoked_action, $this->auth_public_actions)) {
+        	$this->redirect($this->auth_login_url);
+        	return false;
+    	}
+    	return true;
+    }
+
+}
