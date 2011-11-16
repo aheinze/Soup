27142648ddb30d02339cea9fceb38dee84182b08
Artur Heinze <artur.heinze@yootheme.com>
2011-11-16 14:19:12 +0100

    zurb foundation added

--- app/modules/App/views/Start/index.php
+++ app/modules/App/views/Start/index.php
@@ -1,25 +1,29 @@
 <?php $extend("views:default.php");?>
 
-<h2>
-	<span class="strong">Congratulations</span><br /> 
-	<span class="soft">you're ready to start coding your app.</span>
-</h2>
 
-<div style="margin-bottom:30px;">
-	<h3>Getting started</h3>
+<div class="row">
 
-	<p>
-		<ul class="list">
-			<li>Please make sure that the folder <span class="marked">app/tmp</span> and its sub-folders are writable.</li>
-			<li>You can find the app configuration in  <span class="marked">app/config/bootstrap.php</span></li>
-			<li>You can find the default layout in <span class="marked">app/views/default.php</span></li>
-			<li>You can find the controller for this page in <span class="marked">app/modules/App/Controller/Start.php</span></li>
-			<li>You can find the view for this page in <span class="marked">app/modules/App/views/Start/index.php</span></li>
-		</ul>
-	</p>
+	<h2>
+		<span>Congratulations</span>
+		<br /><br /> 
+		<span class="soft">
+			You installed a powerful, and flexible framework for building prototypes and production code.
+			And yes, you're ready to start coding your app.
+		</span>
+	</h2>
+
+	<div class="panel" style="margin:30px 0px;">
+		<h3>Getting started</h3>
+
+		<p>
+			<ul class="nice">
+				<li>Please make sure that the folder <span class="label mark">app/tmp</span> and its sub-folders are writable.</li>
+				<li>You can find the app configuration in  <span class="label mark">app/config/bootstrap.php</span></li>
+				<li>You can find the default layout in <span class="label mark">app/views/default.php</span></li>
+				<li>You can find the controller for this page in <span class="label mark">app/modules/App/Controller/Start.php</span></li>
+				<li>You can find the view for this page in <span class="label mark">app/modules/App/views/Start/index.php</span></li>
+			</ul>
+		</p>
+	</div>
+
 </div>
-
-<h3>Do you need help?</h3> 
-<p>
-	<a href="http://d-xp.com" class="button gray">Contact us!</a>
-</p>
--- app/views/default.php
+++ app/views/default.php
@@ -6,34 +6,23 @@
 	<script type="text/javascript" src="<?php $this->url("/assets/main.js");?>"></script>
 </head>
 <body>
-	<div id="frame">
-		<div id="header">
-			<div class="wrapper">
-				<span id="logo">
-					Raww2 <span>framework</span>
-				</span>
+	<div id="header">
+		<div class="row">
+			<div class="five columns">
+				<h1 id="logo">
+					Raww2 <span class="soft">Less enterprise more fun</span>
+				</h1>
+			</div>
+			<div class="seven columns">
 				<ul id="topnav">
 					<li><a href="https://github.com/aheinze/Raww2">Documentation</a></li>
 					<li><a href="https://github.com/aheinze/Raww2">Github</a></li>
 				</ul>
 			</div>
 		</div>
-		<div id="content">
-			<div class="wrapper">
-				<div id="topbox">
-					<div class="p-20">
-						<div class="h1">Less Enterprise</div>
-						<div class="h2">more fun!</div>
-					</div>
-					<div id="ribbonbar">
-						&mdash; because we &hearts; it simple &mdash;
-						<span class="left"></span>
-						<span class="right"></span>
-					</div>
-				</div>
-				<div id="output"><?php echo $content_for_layout;?></div>
-			</div>
-		</div>
+	</div>
+	<div id="content">
+		<div id="output"><?php echo $content_for_layout;?></div>
 	</div>
 </body>
 </html>
