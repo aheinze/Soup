2c0b10505a6a3e78adde17639e11f199683b7e6c
Artur Heinze <artur.heinze@yootheme.com>
2011-11-22 11:11:59 +0100

    jquery updated

--- app/modules/App/views/Start/index.php
+++ app/modules/App/views/Start/index.php
@@ -2,28 +2,40 @@
 
 
 <div class="row">
+	
+	<div class="eight columns">
+		<h2> Congratulations</h2>
 
-	<h2>
-		Congratulations
-	</h2>
+		<h3 class="soft">
+			You installed a powerful, and flexible framework for building prototypes and production code.
+			And yes, you're ready to start coding your app.
+		</h3>
 
-	<h3 class="soft">
-		You installed a powerful, and flexible framework for building prototypes and production code.
-		And yes, you're ready to start coding your app.
-	</h3>
+		<div class="panel" style="margin:30px 0px;">
+			<h3>Getting started</h3>
 
-	<div class="panel" style="margin:30px 0px;">
-		<h3>Getting started</h3>
-
-		<p>
-			<ul class="nice">
-				<li>Please make sure that the folder <span class="label mark">app/tmp</span> and its sub-folders are writable.</li>
-				<li>You can find the app configuration in  <span class="label mark">app/config/bootstrap.php</span></li>
-				<li>You can find the default layout in <span class="label mark">app/views/default.php</span></li>
-				<li>You can find the controller for this page in <span class="label mark">app/modules/App/Controller/Start.php</span></li>
-				<li>You can find the view for this page in <span class="label mark">app/modules/App/views/Start/index.php</span></li>
-			</ul>
-		</p>
+			<p>
+				<ul class="nice">
+					<li>Please make sure that the folder <span class="label mark">app/tmp</span> and its sub-folders are writable.</li>
+					<li>You can find the app configuration in  <span class="label mark">app/config/bootstrap.php</span></li>
+					<li>You can find the default layout in <span class="label mark">app/views/default.php</span></li>
+					<li>You can find the controller for this page in <span class="label mark">app/modules/App/Controller/Start.php</span></li>
+					<li>You can find the view for this page in <span class="label mark">app/modules/App/views/Start/index.php</span></li>
+				</ul>
+			</p>
+		</div>
 	</div>
+	<div class="four columns">
+		
+		<div class="bordered-panel">
+			<p>
+				<strong>Raww</strong> is sponsered by <a href="http://d-xp.com">d-xp.com</a>
+			</p>
 
+			<p class="soft">
+				Do you have any questions or need support, please contact us.
+			</p>
+		</div>
+
+	</div>
 </div>
--- app/views/default.php
+++ app/views/default.php
@@ -2,19 +2,19 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Raww - framework</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
 	<link rel="stylesheet" type="text/css" href="<?php $this->url("/assets/main.css");?>" />
 	<script type="text/javascript" src="<?php $this->url("/assets/main.js");?>"></script>
 </head>
 <body>
 	<div id="header">
 		<div class="row">
-			<div class="five columns">
-				<h1 id="logo">
-					Raww2 <span class="soft">Less enterprise more fun</span>
-				</h1>
+			<div class="six columns">
+				<h1 id="logo">Raww</h1>
 				
 			</div>
-			<div class="seven columns">
+			<div class="six columns">
 				<ul id="topnav">
 					<li><a href="https://github.com/aheinze/Raww2">Documentation</a></li>
 					<li><a href="https://github.com/aheinze/Raww2">Github</a></li>
