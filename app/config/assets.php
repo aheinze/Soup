f34ade4b0e3776cc3190c7a9841fe5f449d8b090
Artur <artur@d-xp.com>
2011-11-24 00:28:22 +0100

    backbone.js added

--- app/config/assets.php
+++ app/config/assets.php
@@ -2,8 +2,14 @@
 
 /* Assets */
 
+// jQuery reference
 $app["assets"]->addReference("jquery", array(
-	"file" => "root:public/js/jquery.js"
+	"file" => "root:public/js/vendor/jquery.js"
+));
+
+// Backbone reference
+$app["assets"]->addReference("backbone", array(
+	"file" => "root:public/js/vendor/backbone.js"
 ));
 
 $app["router"]->bind("#/assets/main\.(css|js)#", function($params) use($app) {
@@ -11,6 +17,7 @@ $app["router"]->bind("#/assets/main\.(css|js)#", function($params) use($app) {
 	$app["assets"]->register("main", array(
 		//use the jquery reference
 		array("file" => "ref:jquery"),
+		array("file" => "ref:backbone"),
 		array("file" => "root:public/css/base.css"),
 		array("file" => "root:public/css/app.css"),
 	));
