1020c9d6f7642e75e967c0ba5d8d09bf31292069
Artur Heinze <artur@d-xp.com>
2012-03-01 17:36:11 +0100

    router->auto_route description added

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,6 +4,14 @@
 
 
 # define auto routes
+
+/*
+	
+	$app["router"]->auto_route("/blog", __DIR__.'/../bundles');
+
+	/blog/posts/show/34 will load __DIR__.'/../bundles/Blog/Controller/Posts' and call the action "show" with the parameter 34
+*/
+
 $app["router"]->auto_route("/app", __DIR__.'/../bundles');
 
 # define special routes
