fb90f2d38344e32bc4e688eed7f42fa69c6574de
Artur <artur@d-xp.com>
2012-03-02 01:45:23 +0100

    cleanup

--- app/bundles/App/Controller/Start.php
+++ app/bundles/App/Controller/Start.php
@@ -9,12 +9,6 @@ class Start extends \Soup\Controller {
 
     public function index(){
 		
-    	$redis = $this->db("redis");
-
-    	$redis->set('key232', 45);
-
-    	var_dump($redis->get("key232"));
-
         return $this->render(__DIR__."/../views/Start/index.php");
     }
 
